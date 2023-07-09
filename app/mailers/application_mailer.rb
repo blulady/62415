a2b956e697018e2ce099a5a43e0c90dbb6c8db7a
blulady <blulady@gmail.com>
2023-07-09 15:34:53 -0700

    updated template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: "ssanger2020@gmail.com"
   layout "mailer"
   def send_email
-    mail(to:"ssanger@protonmail.com", subject:"sending an email", body:"something")
+    mail(
+      to:"ssanger@protonmail.com", 
+      subject:"sending an email", 
+      body:"Greetings, \nI am reaching out to let you know that I have completed the challange.\nThank you for giving me this opportunity. I look foward to hearing from you.\nBest Wishes, \n 
+      Sarah Sanger"
+      )
   end
 end
