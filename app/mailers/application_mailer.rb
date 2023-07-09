58f7b756837080dc8ba584fc38d229573893aa8a
blulady <blulady@gmail.com>
2023-07-09 13:24:31 -0700

    adding functionality to mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "ssanger2020@gmail.com"
   layout "mailer"
+  def send_email
+    mail(to:"ssanger@protonmail.com, subject:"sending an email")
+  end
 end
