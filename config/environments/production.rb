b727826114f6ea6b5b8c035904d559df22f53a3c
blulady <blulady@gmail.com>
2023-07-10 12:25:59 -0700

    still updating production server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :async
-  # config.active_job.queue_name_prefix = "support_eng_production"
+  config.active_job.queue_name_prefix = "support_eng_production"
 
   config.action_mailer.perform_caching = false
 
